41e2a36c4ee04cbeb45d160e22522526da43e86f
AmanDeep9925 <amandeep58257@gmail.com>
2021-04-19 21:44:18 +0530

    Understanding Props
    Learning and the concepts of the props and
    vizualising to use it

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,49 @@
 import React from "react";
+import ExpenseItem from "./expense-tracker/Components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {
+            title: "Phone",
+            amount: "14,999",
+            date: new Date(2020, 2, 12),
+        },
+        {
+            title: "Mouse",
+            amount: "1,999",
+            date: new Date(2020, 4, 2),
+        },
+        {
+            title: "Etnernet Adapter",
+            amount: "999",
+            date: new Date(2020, 5, 20),
+        },
+        {
+            title: "Key Board",
+            amount: "1,999",
+            date: new Date(2020, 12, 1),
+        },
+    ];
+
+    expenses.map((expense) => console.log(expense));
+
     return (
         <div className="App">
             <h1>Learning ReactJS</h1>
-            <p>Another line in react</p>
+
+            {expenses.map((expense) => (
+                <ExpenseItem
+                    title={expense.title}
+                    date={expense.date}
+                    amount={expense.amount}
+                />
+            ))}
+
+            <ExpenseItem
+                title={expenses[0].title}
+                date={expenses[0].date}
+                amount={expenses[0].amount}
+            />
         </div>
     );
 }
