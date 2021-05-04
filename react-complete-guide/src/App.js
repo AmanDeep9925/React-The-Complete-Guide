a348643b79807e6a70ea16abda506bf805046a35
AmanDeep9925 <amandeep58257@gmail.com>
2021-05-04 12:22:21 +0530

    Expense Form
    Added the expense form for adding a new expense in the expense item list

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Expenses from "./expense-tracker/Components/Expenses/Expenses";
+import NewExpense from "./expense-tracker/Components/NewExpense/NewExpense";
 
 function App() {
     const expenses = [
         {
+            id: "0",
             title: "Phone",
             amount: "14,999",
             date: new Date(2020, 2, 12),
         },
         {
+            id: "1",
             title: "Mouse",
             amount: "1,999",
             date: new Date(2020, 4, 2),
         },
         {
+            id: "2",
             title: "Etnernet Adapter",
             amount: "999",
             date: new Date(2020, 5, 20),
         },
         {
+            id: "3",
             title: "Key Board",
             amount: "1,999",
             date: new Date(2020, 12, 1),
@@ -29,6 +34,7 @@ function App() {
 
     return (
         <div className="App">
+            <NewExpense />
             <Expenses expenses={expenses} />
         </div>
     );
--- react-complete-guide/src/expense-tracker/Components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/expense-tracker/Components/Expenses/ExpenseItem.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+    // Intializing the useState hook for the handling the
+    // click handler
+
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle("Updated🚀");
+        console.log(title);
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">₹ {props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
