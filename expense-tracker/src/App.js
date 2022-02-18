4d554be10589ad0498da9ad6561c6df537c373c9
AmanDeep <amandeep58257@gmail.com>
2022-02-18 21:50:59 +0530

    Updated the folder structure

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/NewExpense/NewExpense";
+
+let DUMMY_EXPENSES = [
+  {
+    id: "0",
+    title: "Phone",
+    amount: "14,999",
+    date: new Date(2020, 2, 12),
+  },
+  {
+    id: "1",
+    title: "Mouse",
+    amount: "1,999",
+    date: new Date(2020, 4, 2),
+  },
+  {
+    id: "2",
+    title: "Etnernet Adapter",
+    amount: "999",
+    date: new Date(2020, 5, 20),
+  },
+  {
+    id: "3",
+    title: "Key Board",
+    amount: "1,999",
+    date: new Date(2020, 12, 1),
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewExpense onAddExpenseHandler={addExpenseHandler} />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
